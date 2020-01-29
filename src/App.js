c51be57cf424129aa0ac89047990e4b78e59a9e2
Mohammed Yousuf <36731361+myousuf07@users.noreply.github.com>
2020-01-29 16:51:27 +0530

    datafile and heading extraction

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
-import React from "react";
-import "./App.css";
+import React, { useState } from "react";
 function App() {
   let inclusions = [];
+  let newinclusion;
   let exclusions = [];
+  let heading = [];
+
   let highlight = fetch("./data/highlight.json")
     .then(res => res.json())
     .then(data => data)
     .catch(err => console.error(err));
+
   let data_file = fetch("./data/data_file.json")
     .then(res => res.json())
     .then(data => data)
     .catch(err => console.error(err));
+
   data_file.then(data => {
     highlight.then(highlighted => {
       let annotations = highlighted.map(item => {
@@ -28,14 +32,40 @@ function App() {
 
       console.log(data);
       console.log(inclusions);
+
       for (let value of Object.values(data.deIdentifiedFile)) {
         if (inclusions.indexOf(value) >= 0) console.log(value);
       }
     });
   });
+  data_file.then(data => {
+    for (var key in data.deIdentifiedFile) {
+      // if (data.deIdentifiedFile.hasOwnProperty(key)) {
+      //   while (typeof data.deIdentifiedFile[key] === "object") {
+      //     var files = data.deIdentifiedFile[key];
+      //     for (var key in files) {
+      //       if (files.hasOwnProperty(key)) {
+      //         console.log(key + " -> " + files[key]);
+      //       }
+      //     }
+      //   }
+      console.log(key + " -> " + data.deIdentifiedFile[key]);
+      heading.push(key);
+    }
+  });
+  const [tablehead, settablehead] = useState(heading);
+  console.log(tablehead);
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <table style={{ width: "100%" }}>
+        <thead>
+          <tr>
+            <th>hello</th>
+            <th>hello</th>
+            <th>hello</th>
+          </tr>
+        </thead>
+      </table>
     </div>
   );
 }
